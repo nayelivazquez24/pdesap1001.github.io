c983fc0a0a5ac7517be7f073cb1b6e89bb6bb97d
pdesap1001 <pdesap1001@gmail.com>
2021-11-16 10:42:22 -0600

    Cambie el init

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+  apiKey: "AIzaSyDOYMpp68_udyaxhGI6xbtyF0n2HURHnP4",
+  authDomain: "fbauten1001.firebaseapp.com",
+  projectId: "fbauten1001",
+  storageBucket: "fbauten1001.appspot.com",
+  messagingSenderId: "717078882003",
+  appId: "1:717078882003:web:c4c3e1e5a917385cced626",
+  measurementId: "G-3QL4D2NXDN"
 });
